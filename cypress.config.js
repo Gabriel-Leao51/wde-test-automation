47ebb7b63d48cd443f623095f1421dccccb09f81
Gabriel Leão <111463641+Gabriel-Leao51@users.noreply.github.com>
2025-03-28 16:52:59 -0300

    ci: Integrate Mochawesome HTML reporter

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,18 @@ const {
 } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter", // Define o reporter a ser usado
+  reporterOptions: {
+    charts: true, // Exibe gráficos no relatório
+    reportPageTitle: "WDE Shop - Relatório de Testes Automatizados", // Título customizado da página HTML
+    embeddedScreenshots: true, // Embutir screenshots diretamente no relatório (se houver falhas)
+    inlineAssets: true, // Inclui CSS/JS no HTML (arquivo único, mais fácil de arquivar)
+    saveAllAttempts: false, // Se usar retries, salva apenas a última tentativa no relatório
+    reportDir: "cypress/reports/mochawesome", // Diretório onde os JSONs serão salvos (IMPORTANTE)
+    overwrite: false, // NÃO sobrescrever relatórios JSON antigos (necessário para merge)
+    html: false, // NÃO gerar HTML automaticamente por arquivo (faremos isso após merge)
+    json: true, // GERAR os arquivos JSON individuais por arquivo de teste
+  },
   video: true,
   e2e: {
     specPattern: "**/*.feature",
