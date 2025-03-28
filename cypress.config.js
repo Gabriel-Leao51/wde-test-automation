7a949cda3e1acac7b78c61a93ef30a0b92e388c4
Gabriel Leão <111463641+Gabriel-Leao51@users.noreply.github.com>
2025-03-28 16:24:59 -0300

    ci: Update video upload config

--- cypress.config.js
+++ cypress.config.js
@@ -8,10 +8,15 @@ const {
 } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
     specPattern: "**/*.feature",
     baseUrl: "https://wde-5p3f.onrender.com/",
     pageLoadTimeout: 90000,
+    retries: {
+      runMode: 1,
+      openMode: 0,
+    },
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
 
